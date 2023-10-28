6d4a794b60c75679369ebd46f1246dd99d639e2f
Jasmine <114477432+Kinaata@users.noreply.github.com>
2023-10-28 23:18:03 +0530

    Add files via upload
    Used else if for creating this Java program.

---
+++ maximumorlargestof3nums.java
@@ -0,0 +1,15 @@
+public class maximumorlargestof3nums{
+    public static void main(String[] args){
+    int a=100;
+    int b=200;
+    int c=50;
+    if(a>b && a>c){
+    System.out.print(a+" is the largest number");}
+    else if(b>c){
+        System.out.print(b+" is the largest number");
+    }
+    else{
+        System.out.print(c+" is the largest number");
+    }    
+    }
+}
