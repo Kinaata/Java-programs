ff6349b2ac440bc143356cdaf5588bbaf49038e5
Jasmine <114477432+Kinaata@users.noreply.github.com>
2023-10-31 11:46:29 +0530

    displaying the elements of array. Used for loop.

---
+++ arrayforloop.java
@@ -0,0 +1,9 @@
+public class arrayforloop{
+    public static void main(String[] args){
+        int Array[]={8,9,7,6};
+        for(int i=0;i<Array.length;i++){
+            System.out.println(Array[i]);
+        }
+    }
+}
+    
