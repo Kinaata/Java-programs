234f0f43f6a735fc8b5cadca950541a2ac6cf3a7
Jasmine <114477432+Kinaata@users.noreply.github.com>
2023-10-31 14:34:39 +0530

    Displaying elements of 2D array using nested for-loop.

---
+++ MDArray.java
@@ -0,0 +1,11 @@
+public class MDArray{
+    public static void main(String[] args){
+        int Arr[][]={{11,22,33},{44,55,66}};
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(Arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
