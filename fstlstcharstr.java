02cce838bf48b75927b003fe85d642ec3aa7004c
Jasmine <114477432+Kinaata@users.noreply.github.com>
2023-11-07 16:36:01 +0530

    accessing first & last chars using charAt() method

---
+++ fstlstcharstr.java
@@ -0,0 +1,8 @@
+public class fstlstcharstr{
+    public static void main(String[] args){
+        String str="Hii beautiful";
+        int strLength=str.length();
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(strLength-1));
+    }
+}
